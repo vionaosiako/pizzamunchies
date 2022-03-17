b4b1ef71156e7ad5328c97181e8b02706e3a86ef
vionaosiako <viona.osiako@student.morigaschool.com>
2022-03-17 12:35:34 +0300

    Add deliveryLocation function

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function deliveryLocation(){    
+    let location = prompt("Enter delivery location");
+    alert("Delivery fee is: ksh.300");
+    alert("Your order will be delivered to your location: "+location);
+}
