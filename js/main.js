831dbb5709802f76eb3410dcdfcbac8c760aa981
vionaosiako <viona.osiako@student.morigaschool.com>
2022-03-17 19:41:34 +0300

    Add Pizza class in js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,114 @@ function deliveryLocation(){
     alert("Delivery fee is: ksh.300");
     alert("Your order will be delivered to your location: "+location);
 }
+
+class Pizza{
+    constructor(customerName, pizzaSize, crustType, pizzaTopping, pizzaQuantity)
+    {
+        this.customerName = customerName;
+        this.pizzaSize = pizzaSize;
+        this.crustType = crustType;
+        this.pizzaTopping = pizzaTopping;
+        this.pizzaQuantity = pizzaQuantity;
+    }
+    // methods
+    getPriceInSize() {
+        if (this.pizzaSize === "Small") {
+          return 200;
+        }
+        if (this.pizzaSize === "Medium") {
+          return 400;
+        }
+        if (this.pizzaSize === "Large") {
+          return 700;
+        }
+      }
+    
+      getCrustPrice() {
+        if (this.crustType === "Crispy") {
+          return 100;
+        }
+        if (this.crustType === "Staffed") {
+          return 200;
+        }
+        if (this.crustType === "Thin-crust") {
+          return 300;
+        }
+        if (this.crustType === "Thick-crust") {
+          return 400;
+        }
+      }
+    
+      getPriceInToppings() {
+        if (this.pizzaTopping === "Pepperoni") {
+          return 100;
+        }
+        if (this.pizzaTopping === "Mushroom") {
+          return 200;
+        }
+        if (this.pizzaTopping === "Barbeque") {
+          return 300;
+        }
+        if (this.pizzaTopping === "Extra-Cheese") {
+          return 400;
+        }
+        if (this.pizzaTopping === "Vegetable") {
+          return 500;
+        }
+        if (this.pizzaTopping === "Pineapple") {
+          return 600;
+        }
+        if (this.pizzaTopping === "Chicken") {
+          return 700;
+        }
+        if (this.pizzaTopping === "Black-olive") {
+          return 800;
+        }
+      }
+
+      getTotalPrice() {
+        return (
+          (this.getPriceInSize() +
+            this.getCrustPrice() +
+            this.getPriceInToppings()) *
+          this.pizzaQuantity
+        );
+      }
+    
+}//End of Pizza class
+
+let submitBtn = document.getElementById("orderBtn");
+submitBtn.addEventListener('click',function(){
+    const pizza = new Pizza();
+    pizza.name = document.getElementById("customerName").value;
+    pizza.pizzaSize = document.getElementById("pizzaSize").value;
+    pizza.crustType = document.getElementById("crustType").value;
+    pizza.pizzaTopping = document.getElementById("pizzaTopping").value;
+    pizza.pizzaQuantity = parseInt(document.getElementById("pizzaQuantity").value);
+
+    let total = pizza.getTotalPrice();
+    // console.log(total);
+
+    // document.getElementById("total").innerHTML =`The total price of ${pizza.pizzaQuantity} for ${
+    //     pizza.name
+    //   } is ${pizza.getTotalPrice()}`
+
+    document.getElementById("total").innerHTML = total;
+
+        // const pizza = new Pizza(customerName, pizzaSize, crustType, pizzaTopping, pizzaQuantity);
+    
+        // console.log(pizza.name);
+        // console.log(pizza.pizzaSize);
+        // console.log(pizza.crustType);
+        // console.log(pizza.pizzaTopping);
+        // console.log(pizza.pizzaQuantity);
+
+        console.log(pizza.getpizzaTopings);
+    
+})
+
+
+
+
+
+
